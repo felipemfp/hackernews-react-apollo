ce56b489acc0d909c3a099ebe0b21dc9047def9f
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-20 20:42:47 -0300

    Add FeedQuery to LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import Link from './Link'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class LinkList extends Component {
 
   render() {
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>
+    }
 
-    const linksToRender = [{
-      id: '1',
-      description: 'The coolest GraphQL backend 😎',
-      url: 'https://www.graph.cool'
-    }, {
-      id: '2',
-      description: 'The best GraphQL Client',
-      url: 'http://dev.apollodata.com/'
-    }]
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.feedQuery.feed.links
 
     return (
       <div>
@@ -26,4 +27,17 @@ class LinkList extends Component {
 
 }
 
-export default LinkList
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
